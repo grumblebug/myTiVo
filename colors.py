88645f0a71874e0e6131050658e1f2be24f2f836
grumblebug <filmfan_man@yahoo.com>
2015-03-03 13:40:50 -0500

    Exception for files not existing does not work

--- colors.py
+++ colors.py
@@ -16,25 +16,25 @@ sourcefile = "content/%s" % colorval
 destinfile = "content/main.css"
 
 #See if the files exist
-li = [sourcefile, destinfile]
-for checkfile in li:
-    try:
-        with open ('checkfile') as file:
-            pass
-        except IOError as e:
-            print "Content-type:text/html\r\n\r\n"
-            print "<html>"
-            print "<head>"
-            print "<title>Exception</title>"
-            print """<link rel="stylesheet" href="/main.css" type="text/css">"""
-            print "<body>"
-            print "<img src=stop.png><h3>Unable to find %s </3>" % checkfile
-            print "</body>"
-            print "</html>"
+# li = [sourcefile, destinfile]
+#for checkfile in li:
+#    try:
+#        with open ('checkfile') as file:
+#          pass
+#        except IOError as e:
+#            print "Content-type:text/html\r\n\r\n"
+#            print "<html>"
+#            print "<head>"
+#            print "<title>Exception</title>"
+#            print """<link rel="stylesheet" href="/main.css" type="text/css">"""
+#            print "<body>"
+#            print "<img src=stop.png><h3>Unable to find %s </3>" % checkfile
+#            print "</body>"
+#            print "</html>"
 
 # Copy the new color scheme file to main.css
 copyfile(sourcefile, destinfile)
-
+k
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
